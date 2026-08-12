22c488f2d503db68050ec40cd3b59239abdfc755
Alex <450145524@qq.com>
2026-08-12 17:21:59 +0800

    feat: add account types

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface AccountMeta {
+  email?: string;
+  plan?: string;
+  weekLeft?: number;
+  weekReset?: number;
+  updatedAt?: string;
+}
+
+export interface AccountProfile {
+  name: string;
+  isCurrent: boolean;
+  meta?: AccountMeta;
+}
